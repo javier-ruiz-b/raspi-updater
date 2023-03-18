46f47b1f80eca350c0f4a9e7dd96e381d9f60ce1
Javier R <triomphe3@gmail.com>
2023-03-18 22:59:45 +0100

    Fixed script

--- build-and-rebuild-initramfs.sh
+++ build-and-rebuild-initramfs.sh
@@ -3,7 +3,9 @@ set -euxo pipefail
 
 cd "$(dirname "$(realpath "$0")")/debian-packaging"
 
-./build.sh
+
+arch="$(dpkg --print-architecture)"
+./build.sh "$arch"
 sudo dpkg -i output/raspi-*_amd64.deb
 sudo update-initramfs -u -k "$(uname -r)"
 sudo update-grub
