2bbecdc3b23e91a983cd0c24ed7e21bf9e0bebc6
Javier Ruiz <javier.ruiz@outlook.de>
2023-03-22 18:34:07 +0100

    Cleaned up, fixed config

--- run-server.sh
+++ run-server.sh
@@ -9,7 +9,9 @@ PATH="$PATH:$(pwd)/tools_win"
 mkdir -p "$src_dir/server_images"
 
 cd "cmd/updater"
-go run *.go \
+
+GOOS=windows GOARCH=amd64 go build -race -o windows-amd64 
+./windows-amd64 \
     -address "0.0.0.0:31416" \
     -certFile "$src_dir/pkg/testdata/cert.pem" \
     -keyFile "$src_dir/pkg/testdata/priv.key" \
