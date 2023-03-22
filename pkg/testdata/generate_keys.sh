b7a8b86857af13c2a3fa111d32aa2dc7a69dffab
Javier Ruiz <javier.ruiz@outlook.de>
2023-03-22 18:46:59 +0100

    keys have a hostname

---
+++ pkg/testdata/generate_keys.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eux
+
+hostname="$1"
+
+echo "Generating CA key and certificate:"
+openssl req -x509 -sha256 -nodes -days 3650 -newkey rsa:4096 \
+  -keyout ca.key -out ca.crt \
+  -subj '/O=quic-go Certificate Authority/'
+
+echo "Generating CSR"
+openssl req -out "$hostname".csr -new -newkey rsa:4096 -nodes -keyout "$hostname".key \
+  -subj '/O=quic-go/'
+
+echo "Sign certificate:"
+openssl x509 -req -sha256 -days 3650 -in "$hostname".csr  -out "$hostname".crt \
+  -CA ca.crt -CAkey ca.key -CAcreateserial \
+  -extfile <(printf "subjectAltName=DNS:localhost")
+
+# debug output the certificate
+openssl x509 -noout -text -in "$hostname".crt
+
+# we don't need the CA key, the serial number and the CSR anymore
+rm ca.key "$hostname".csr ca.srl
+
